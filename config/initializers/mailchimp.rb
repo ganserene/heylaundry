244dad912289e005a47f9b4767dfa3d58f14d814
Jia Shern <jiashern@hotmail.com>
2015-01-26 17:12:30 +0800

    update config gibbon

--- config/initializers/mailchimp.rb
+++ config/initializers/mailchimp.rb
@@ -3,4 +3,8 @@ if Rails.env.test?
   Gibbon::Export.throws_exceptions = false
 end
 
-Rails.configuration.mailchimp = Gibbon::API.new(ENV['MAILCHIMP_KEY'])
+# Rails.configuration.mailchimp = Gibbon::API.new(ENV['MAILCHIMP_KEY'])
+
+Gibbon::API.api_key = ENV['MAILCHIMP_KEY']
+Gibbon::API.timeout = 15
+Gibbon::API.throws_exceptions = false
