c2169fb2ed62b7835e5827dabfb3d9f83b251b07
Jia Shern <jiashern@hotmail.com>
2015-01-26 17:19:48 +0800

    comment gibbon initialzier

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,10 +1,10 @@
-if Rails.env.test?
-  Gibbon::Export.api_key = "fake"
-  Gibbon::Export.throws_exceptions = false
-end
+# if Rails.env.test?
+#   Gibbon::Export.api_key = "fake"
+#   Gibbon::Export.throws_exceptions = false
+# end
 
-# Rails.configuration.mailchimp = Gibbon::API.new(ENV['MAILCHIMP_KEY'])
+# # Rails.configuration.mailchimp = Gibbon::API.new(ENV['MAILCHIMP_KEY'])
 
-Gibbon::API.api_key = ENV['MAILCHIMP_KEY']
-Gibbon::API.timeout = 15
-Gibbon::API.throws_exceptions = false
+# Gibbon::API.api_key = ENV['MAILCHIMP_KEY']
+# Gibbon::API.timeout = 15
+# Gibbon::API.throws_exceptions = false
